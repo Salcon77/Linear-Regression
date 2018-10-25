9c67146f3aa6d4cfb48e1131f74a7fc62a19e186
Salcon77 <Salcon77@users.noreply.github.com>
2018-10-25 18:05:23 -0500

    Testing and Accuracy

--- FormulaTest.py
+++ FormulaTest.py
@@ -1,6 +1,11 @@
 import math
 import pandas as pd
 import quandl
+import numpy as np
+from sklearn import preprocessing, model_selection,svm
+from sklearn.linear_model import LinearRegression
+
+
 pd.set_option('display.max_columns', 10)
 
 
@@ -22,11 +27,26 @@ forcast_col = 'Adj. Close'
 # replace nand data with -99999 it will be treated as an outlier
 df.fillna(-99999, inplace=True)
 
-#!
+# set forcast_out to be a 1% of the len of the dataframe. A dataframe of 100 would have 1% length of 1.
 forcast_out = int(math.ceil(0.01*len(df)))
 
-#!
+# create a label column that returns forcasted values shifted by x days ahead
 df['label']=df[forcast_col].shift(-forcast_out)
 df.dropna(inplace=True)
 
-print(df.tail())
+x = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+x= preprocessing.scale(x)
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(x_train, y_train)
+clf.score(x_test, y_test)
+accuracy = clf.score(x_test,y_test)
+
+print(accuracy)
+
